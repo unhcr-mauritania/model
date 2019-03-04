51cd3bff8929ff58ab0add437341a8fea705962b
Edouard-Legoupil <edouard.legoupil@gmail.com>
2019-03-04 17:49:24 +0200

    add partions within predicted group based on pc

--- run-prioritisation.R
+++ run-prioritisation.R
@@ -46,6 +46,33 @@ forest_prediction$predicted.prob <- ifelse(forest_prediction$predicted.target ==
 
 cases2target <- cbind(cases2,forest_prediction )
 
+## further split using apportion  based on initial population segment -
+data <- cases2target
+# **Emergent** -  12%
+# **Catalyst** -  2%
+## so top 14 % of full allocation
+data.full <- data[data$predicted.target == "full.allocation", c("CaseNo", "predicted.prob")]
+data.full$predicted.target.centile <- ntile(data.full$predicted.prob, 100)
+data.full$predicted.target.sub <- ifelse(data.full$predicted.target.centile >= 14, "full.top14pc","full.tail86pc")
+# **Unstable** -  11%
+# **Fragile** -  19%
+## so top 36 % of full allocation
+data.reduc <- data[data$predicted.target == "reduced.allocation", c("CaseNo", "predicted.prob")]
+data.reduc$predicted.target.centile <- ntile(data.reduc$predicted.prob, 100)
+data.reduc$predicted.target.sub <- ifelse(data.reduc$predicted.target.centile >= 36, "reduc.top36pc","reduc.tail64pc")
+
+# **Destitute** -  34%
+# **Insecure** -  22%
+## so top 39 % of full allocation
+data.no <- data[data$predicted.target == "no.allocation", c("CaseNo", "predicted.prob")]
+data.no$predicted.target.centile <- ntile(data.no$predicted.prob, 100)
+data.no$predicted.target.sub <- ifelse(data.no$predicted.target.centile >= 39, "no.top39pc","no.tail61pc")
+
+datasub <- rbind(data.no, data.full, data.reduc)
+datasub <- datasub[ ,c("caseNo", "predicted.target.sub")]
+cases2target <- merge( x = cases2target, y = datasub, by = "caseNo", all.x = TRUE )
+
+
 prop.table(table(forest_prediction$predicted.target, useNA = "ifany"))
 table(forest_prediction$predicted.target, useNA = "ifany")
 
